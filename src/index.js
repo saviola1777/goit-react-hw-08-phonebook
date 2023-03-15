915aa44e95bc796ec682bcc7b11ed2f5de3fe00f
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-03-15 17:54:38 +0200

    new

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store , persistor  } from './Redux/store'
 import { BrowserRouter } from "react-router-dom";
 
-import { Provider } from 'react-redux'; // імпортуємо компонент провайдер який дає доступ до глобального стану і передає сторе огортуєм App імаємо глобальний доступ до store
+import { Provider } from 'react-redux'; // імпортуємо компонент провайдер який дає доступ до глобального стану і передає сторе огортуєм App імаємо глобальний доступ до store d
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
